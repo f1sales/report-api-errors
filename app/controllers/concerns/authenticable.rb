40d498724d80032b4e3487c8b6435980207e14ec
Nathanael Vieira <nathanael@f1sales.com.br>
2022-07-13 18:28:37 -0300

    Fixing Rubocop errors

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -1,16 +1,16 @@
 module Authenticable
-	private  
-	
-	def authenticate_with_token
-		@token ||= request.headers['Authorization']
+  private
 
-		unless valid_token?
-			render json: { errors: 'Por favor, forneça o token no header Authorization'}, 
-						 status: :unauthorized
+  def authenticate_with_token
+    @token ||= request.headers['Authorization']
+
+    unless valid_token?
+      render json: { errors: 'Por favor, forneça o token no header Authorization' },
+             status: :unauthorized
     end
-	end
+  end
 
-	def valid_token?
-		@token.present? && @token == Rails.application.credentials.token
+  def valid_token?
+    @token.present? && @token == Rails.application.credentials.token
   end
 end
